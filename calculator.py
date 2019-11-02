b3edcbfb5b2612ab679d2a40a8df5daa9df5fb08
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2019-11-02 22:08:11 +0300

    Fix indentation in calculator

--- calculator.py
+++ calculator.py
@@ -2,12 +2,12 @@ x = float(input('Enter your first number: '))
 y = float(input('Enter your second number: '))
 z = input('Enter an operator(+,-,/,*) =')
 if z == '+':
- print(x+y)
+    print(x+y)
 elif z == '-':
- print(x-y)
+    print(x-y)
 elif z == '*':
- print(x*y)
+    print(x*y)
 elif y != '0' and z == '/':
- print(x/y)
+    print(x/y)
 elif y == '0':
- print("ZeroDivisionError")
+    print("ZeroDivisionError")
